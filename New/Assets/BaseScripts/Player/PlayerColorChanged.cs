ed3c9263124568292a215c4fee60cc41595ebe0d
dato-svg <139239441+dato-svg@users.noreply.github.com>
2025-03-16 18:15:05 +0400

    save
    save

--- New/Assets/BaseScripts/Player/PlayerColorChanged.cs
+++ New/Assets/BaseScripts/Player/PlayerColorChanged.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using add;
 using UnityEngine;
 
 namespace BaseScripts.Player
@@ -9,10 +10,22 @@ namespace BaseScripts.Player
         [SerializeField] private Color baseColor;
 
         [SerializeField] private GameObject ITSABAGXAXAA;
+        
+        [SerializeField] private AudioClip errorTouch; 
+        private readonly string _errorTouchString = "ErrorTouch"; 
+        
         private SpriteRenderer _spriteRenderer;
         
         private Coroutine _coroutine;
-
+        
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _spriteRenderer.color = baseColor;
+            AudioManager.Instance.RegisterSound(_errorTouchString, errorTouch);
+            Debug.Log("Register Sound:"  + _errorTouchString);
+        }
+        
         public void StartCoroutiner()
         {
             StopCoroutiner();
@@ -27,14 +40,11 @@ namespace BaseScripts.Player
             StopCoroutine(_coroutine);
         }
         
-        private void Awake()
-        {
-            _spriteRenderer = GetComponent<SpriteRenderer>();
-            _spriteRenderer.color = baseColor;
-        }
         
         private IEnumerator StartColorChanger()
         {
+            Debug.Log("Start ColorChanger");
+            AudioManager.Instance.PlaySound(_errorTouchString,1,false);
             GameObject bug = Instantiate(ITSABAGXAXAA,transform.position,Quaternion.identity);
             _spriteRenderer.color = touchColor;
             yield return new WaitForSeconds(0.3f);
--- New/Assets/add/AudioManager.cs
+++ New/Assets/add/AudioManager.cs
@@ -1,78 +1,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+namespace add
 {
-    public static AudioManager Instance { get; private set; }
-
-    [SerializeField] private AudioSource audioSourcePrefab;
-
-    private Dictionary<string, AudioClip> audioClips = new Dictionary<string, AudioClip>();
-    private List<AudioSource> activeAudioSources = new List<AudioSource>();
-
-    private void Awake()
+    public class AudioManager : MonoBehaviour
     {
-        if (Instance == null)
+        public static AudioManager Instance { get; private set; }
+
+        [SerializeField] private AudioSource audioSourcePrefab;
+
+        private Dictionary<string, AudioClip> audioClips = new Dictionary<string, AudioClip>();
+        private List<AudioSource> activeAudioSources = new List<AudioSource>();
+
+        private void Awake()
         {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
 
    
-    public void RegisterSound(string name, AudioClip clip)
-    {
-        if (!audioClips.ContainsKey(name))
+        public void RegisterSound(string name, AudioClip clip)
         {
-            audioClips[name] = clip;
-        }
-    }
-
-    
-    public void PlaySound(string name, float volume = 1f, bool loop = false)
-    {
-        if (!audioClips.ContainsKey(name))
-        {
-            Debug.LogWarning($"[AudioManager] Звук {name} не найден!");
-            return;
-        }
-
-        AudioSource source = Instantiate(audioSourcePrefab, transform);
-        source.clip = audioClips[name];
-        source.volume = volume;
-        source.loop = loop;
-        source.Play();
-
-        activeAudioSources.Add(source);
-
-        if (!loop) Destroy(source.gameObject, source.clip.length);
-    }
-
-    
-    public void StopAllSounds()
-    {
-        foreach (var source in activeAudioSources)
-        {
-            if (source != null)
-                Destroy(source.gameObject);
-        }
-        activeAudioSources.Clear();
-    }
-
-    
-    public void StopSound(string name)
-    {
-        foreach (var source in activeAudioSources)
-        {
-            if (source != null && source.clip.name == name)
+            if (!audioClips.ContainsKey(name))
             {
-                Destroy(source.gameObject);
-                activeAudioSources.Remove(source);
-                break;
+                audioClips[name] = clip;
+            }
+        }
+
+    
+        public void PlaySound(string name, float volume = 1f, bool loop = false)
+        {
+            if (!audioClips.ContainsKey(name))
+            {
+                Debug.LogWarning($"[AudioManager] пїЅпїЅпїЅпїЅ {name} пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ!");
+                return;
+            }
+
+            AudioSource source = Instantiate(audioSourcePrefab, transform);
+            source.clip = audioClips[name];
+            source.volume = volume;
+            source.loop = loop;
+            source.Play();
+
+            activeAudioSources.Add(source);
+
+            if (!loop) Destroy(source.gameObject, source.clip.length);
+        }
+
+    
+        public void StopAllSounds()
+        {
+            foreach (var source in activeAudioSources)
+            {
+                if (source != null)
+                    Destroy(source.gameObject);
+            }
+            activeAudioSources.Clear();
+        }
+
+    
+        public void StopSound(string name)
+        {
+            foreach (var source in activeAudioSources)
+            {
+                if (source != null && source.clip.name == name)
+                {
+                    Destroy(source.gameObject);
+                    activeAudioSources.Remove(source);
+                    break;
+                }
             }
         }
     }
