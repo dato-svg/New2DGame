6a76dc4708dc80ce4ca9a3a1695256e7695cf947
qartveli-AOC <139194826+qartveli-AOC@users.noreply.github.com>
2025-03-16 23:57:37 +0400

    fix

--- New/Assets/BaseScripts/SceneGod/GameStarter.cs
+++ New/Assets/BaseScripts/SceneGod/GameStarter.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace BaseScripts.SceneGod
 {
     public class GameStarter : MonoBehaviour
     {
-        
+            public void ChangeSce()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                ChangeSce();
+            }
+        }
     }
 }
