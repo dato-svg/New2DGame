aed40a19a1f91b9f42cbfea6ddae8943590c39bb
qartveli-AOC <139194826+qartveli-AOC@users.noreply.github.com>
2025-03-16 17:34:40 +0400

    sound

---
+++ New/Assets/add/SoundPlay.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SoundPlay : MonoBehaviour
+{
+    private AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    PlaySound
+}
